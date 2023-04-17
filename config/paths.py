1c430f61127a3929b5aa04149b9c1c0a5ff80b03
Biophilia <127826463+BiophiliaSWDA@users.noreply.github.com>
2023-04-17 20:19:07 +0800

    Add files via upload

--- config/paths.py
+++ config/paths.py
@@ -16,8 +16,16 @@ tf_tmp_file = Path.joinpath(tf_file, "template")
 tf_mut_file = Path.joinpath(tf_file, "mutate")
 
 # tf变异函数列表(json格式)
-tf_func_file = Path.joinpath(tf_file, "data/function/json")
+tf_func_file = Path.joinpath(tf_file, "data/function")
+
+# tf函数定义相似度
+tf_func_def_file = Path.joinpath(tf_func_file, "def_sim")
+
+# tf函数参数列表相似度
+tf_func_param_file = Path.joinpath(tf_func_file, "param_sim")
+
+# tf函数相似度
+tf_func_sim_file = Path.joinpath(tf_func_file, "sim")
 
 # tf参数变异参数储存位置
 tf_param_file = Path.joinpath(tf_file, "data/param")
-
