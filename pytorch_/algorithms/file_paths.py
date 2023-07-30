7d95f76c496d44b29cd684648a67ab958d831156
LingyueYan-NJU <3532405629@qq.com>
2023-07-31 04:55:51 +0800

    MoCo_PyTorch final version

---
+++ pytorch_/algorithms/file_paths.py
@@ -0,0 +1,16 @@
+import os
+ALGORITHM_PATH = os.path.dirname(__file__)
+MAIN_PATH = os.path.join(ALGORITHM_PATH, '..')
+MODEL_PATH = os.path.join(MAIN_PATH, 'seed_models')
+
+LAYER_INFO_PATH = os.path.join(MAIN_PATH, 'torch_layer_info')
+
+LAYER_SIMILARITY_PATH = os.path.join(MAIN_PATH, 'torch_layer_similarity')
+
+MUTATED_MODEL_PATH = os.path.join(MAIN_PATH, 'mutated_model')
+
+SAVED_MODEL_PATH = os.path.join(MAIN_PATH, 'saved_mutated_models')
+
+LOG_PATH = os.path.join(MAIN_PATH, 'logs')
+
+DATASET_PATH = os.path.join(MAIN_PATH, '..', 'datasets')
---
+++ pytorch_/algorithms/go_Fuzzing.py
@@ -0,0 +1,53 @@
+import os
+import traceback
+
+import file_paths
+from assemble_complex import Assembler_Complex
+import argparse
+
+if __name__ == '__main__':
+    MODEL_LIST = ['ResNet18', 'ResNet50', 'InceptionV3', 'xception', 'testnet',
+                  'alexnet', 'lenet', 'mobilenet', 'squeezenet', 'vgg16', 'vgg19',
+                  'densenet', 'LSTM', 'GRU', 'googlenet', 'LOOP', 'BiLSTM']
+
+    # params: ========
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--MODEL', type=str, default='testnet', help='Model name')
+    parser.add_argument('--N', type=int, default=3, help='N value')
+
+    args = parser.parse_args()
+
+    MODEL = args.MODEL
+    N = args.N
+
+    skip_list = ['LOOP', 'testnet']
+
+    assert MODEL in MODEL_LIST
+
+    if MODEL == 'LOOP':
+        for i in range(5):
+            for model in MODEL_LIST:
+                if model in skip_list:
+                    continue
+                try:
+                    a = Assembler_Complex(model)
+                    a.set_n(N)
+                    a.assemble_code_tree()
+                    os.rename(os.path.join(file_paths.LOG_PATH, model, 'log.txt'),
+                              os.path.join(file_paths.LOG_PATH, model, 'log' + str(i) + '.txt'))
+                except Exception:
+                    error = str(traceback.format_exc())
+                    f = open(os.path.join(file_paths.MAIN_PATH, 'report.txt'), 'a', encoding='utf-8')
+                    f.write(error)
+                    f.close()
+                    skip_list.append(model)
+                    continue
+
+    # ================
+
+    # run
+    else:
+        a = Assembler_Complex(MODEL)
+        a.set_n(N)
+        a.assemble_code_tree()
