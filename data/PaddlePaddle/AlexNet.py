82a13edaf0d0f794a4f72b68024611a856acef6b
Chen <1731140705@qq.com>
2023-10-23 22:44:46 +0800

    modify pytorch/torch_layer_info

--- data/PaddlePaddle/AlexNet.py
+++ data/PaddlePaddle/AlexNet.py
@@ -1,47 +1,32 @@
-%matplotlib inline
 import paddle
-import paddle.fluid as fluid
-import numpy as np
-import matplotlib.pyplot as plt
-from paddle.vision.datasets import Cifar10, Cifar100
-from paddle.vision.transforms import Transpose
-from paddle.io import Dataset, DataLoader
-from paddle import nn
-import paddle.nn.functional as F
-import paddle.vision.transforms as transforms
-import os
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import figure
-import paddlex
-from paddle import ParamAttr
 
 
-class AlexNet(nn.Layer):
+class AlexNet(paddle.nn.Layer):
     def __init__(self, num_classes=10):
         super().__init__()
-        self.features = nn.Sequential(
-            nn.Conv2D(3, 48, kernel_size=11, stride=4, padding=11 // 2),
-            nn.ReLU(),
-            nn.MaxPool2D(kernel_size=3, stride=2),
-            nn.Conv2D(48, 128, kernel_size=5, padding=2),
-            nn.ReLU(),
-            nn.MaxPool2D(kernel_size=3, stride=2),
-            nn.Conv2D(128, 192, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.Conv2D(192, 192, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.Conv2D(192, 128, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2D(kernel_size=3, stride=2),
+        self.features = paddle.nn.Sequential(
+            paddle.nn.Conv2D(3, 48, kernel_size=11, stride=4, padding=11 // 2),
+            paddle.nn.ReLU(),
+            paddle.nn.MaxPool2D(kernel_size=3, stride=2),
+            paddle.nn.Conv2D(48, 128, kernel_size=5, padding=2),
+            paddle.nn.ReLU(),
+            paddle.nn.MaxPool2D(kernel_size=3, stride=2),
+            paddle.nn.Conv2D(128, 192, kernel_size=3, stride=1, padding=1),
+            paddle.nn.ReLU(),
+            paddle.nn.Conv2D(192, 192, kernel_size=3, stride=1, padding=1),
+            paddle.nn.ReLU(),
+            paddle.nn.Conv2D(192, 128, kernel_size=3, stride=1, padding=1),
+            paddle.nn.ReLU(),
+            paddle.nn.MaxPool2D(kernel_size=3, stride=2),
         )
-        self.classifier = nn.Sequential(
-            nn.Linear(3 * 3 * 128, 2048),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(2048, 2048),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(2048, num_classes),
+        self.classifier = paddle.nn.Sequential(
+            paddle.nn.Linear(3 * 3 * 128, 2048),
+            paddle.nn.ReLU(),
+            paddle.nn.Dropout(0.5),
+            paddle.nn.Linear(2048, 2048),
+            paddle.nn.ReLU(),
+            paddle.nn.Dropout(0.5),
+            paddle.nn.Linear(2048, num_classes),
         )
 
     def forward(self, x):
--- data/PaddlePaddle/BA_AlexNet.py
+++ data/PaddlePaddle/BA_AlexNet.py
@@ -1,42 +1,28 @@
-%matplotlib inline
 import paddle
-import paddle.fluid as fluid
-import numpy as np
-import matplotlib.pyplot as plt
-from paddle.vision.datasets import Cifar10, Cifar100
-from paddle.vision.transforms import Transpose
-from paddle.io import Dataset, DataLoader
-from paddle import nn
-import paddle.nn.functional as F
-import paddle.vision.transforms as transforms
-import os
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import figure
-import paddlex
-from paddle import ParamAttr
 
-class BA_module(nn.Layer):
+
+class BA_module(paddle.nn.Layer):
     def __init__(self, pre_channels, cur_channel, reduction=16):
         super().__init__()
-        self.pre_fusions = nn.LayerList(
-            [nn.Sequential(
-                nn.AdaptiveAvgPool2D(1),
-                nn.Conv2D(pre_channel, cur_channel // reduction, 1, bias_attr=False),
-                nn.BatchNorm2D(cur_channel // reduction)
+        self.pre_fusions = paddle.nn.LayerList(
+            [paddle.nn.Sequential(
+                paddle.nn.AdaptiveAvgPool2D(1),
+                paddle.nn.Conv2D(pre_channel, cur_channel // reduction, 1, bias_attr=False),
+                paddle.nn.BatchNorm2D(cur_channel // reduction)
             )
                 for pre_channel in pre_channels]
         )
 
-        self.cur_fusion = nn.Sequential(
-                nn.AdaptiveAvgPool2D(1),
-                nn.Conv2D(cur_channel, cur_channel // reduction, 1, bias_attr=False),
-                nn.BatchNorm2D(cur_channel // reduction)
-            )
+        self.cur_fusion = paddle.nn.Sequential(
+            paddle.nn.AdaptiveAvgPool2D(1),
+            paddle.nn.Conv2D(cur_channel, cur_channel // reduction, 1, bias_attr=False),
+            paddle.nn.BatchNorm2D(cur_channel // reduction)
+        )
 
-        self.generation = nn.Sequential(
-            nn.ReLU(),
-            nn.Conv2D(cur_channel // reduction, cur_channel, 1, bias_attr=False),
-            nn.Sigmoid()
+        self.generation = paddle.nn.Sequential(
+            paddle.nn.ReLU(),
+            paddle.nn.Conv2D(cur_channel // reduction, cur_channel, 1, bias_attr=False),
+            paddle.nn.Sigmoid()
         )
 
     def forward(self, pre_layers, cur_layer):
